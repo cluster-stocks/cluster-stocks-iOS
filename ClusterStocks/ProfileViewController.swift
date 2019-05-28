d2faed6cbf07053ba4c58fcc101206111b14cafc
sushmitha <ganeshsushmitha94@gmail.com>
2019-05-27 20:37:37 -0700

    Basic app structure

---
+++ ClusterStocks/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ClusterStocks
+//
+//  Created by Sushmitha on 5/27/19.
+//  Copyright © 2019 Games. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
